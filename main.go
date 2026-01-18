9560cb7e4ce43aba177ddfb565389899629a9c59
Usman Zahid <developerusman@yahoo.com>
2026-01-18 21:10:16 +0500

    Finalized the server

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -12,29 +12,29 @@ type Response struct {
 }
 
 func main() {
-	http.HandleFunc("/", func(reponseWriter http.ResponseWriter, request *http.Request) {
-		// Every un-registered will result in 404
-		if request.URL.Path != "/" {
-			http.NotFound(reponseWriter, request)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Handles the un-registered routes
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
 			return
 		}
-	
-		reponseWriter.Header().Set("Content-Type", "text/html")
-		fmt.Fprintf(reponseWriter, "<p>Server up and running!</p>")
+		
+		w.Header().Set("Content-Type", "text/html")
+		w.Write([]byte("<p>Server up and running!</p>"))
 	})
 
-	http.HandleFunc("/api", func(reponseWriter http.ResponseWriter, request *http.Request) {
-		reponseWriter.Header().Set("Content-Type", "application/json")
+	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
 		resp := Response{
 			Message: "Server is up and running!",
 			ResponseCode: 200,
 		}
-		json.NewEncoder(reponseWriter).Encode(resp)
+		json.NewEncoder(w).Encode(resp)
 	})
 
-	fmt.Println("Server running at http://localhost:8080")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Println("Server error:", err)
+	serverAddr := ":8080"
+	log.Printf("Server running at http://localhost%s\n", serverAddr)
+	if err := http.ListenAndServe(serverAddr, nil); err != nil {
+		log.Fatal("Server error:", err)
 	}
 }
