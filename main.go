befd4e9ec9b4403a3fd516f80d63b92426f230c3
Usman Zahid <developerusman@yahoo.com>
2026-01-18 20:22:51 +0500

    A simple greetings program

--- main.go
+++ main.go
@@ -2,23 +2,34 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
-func main() {	
+func main() {
 	name := readName()
-	
-    fmt.Println("Hello,", name)
+	fmt.Println("Hello,", name)
+
+	// Set up a simple HTTP route
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %s!", name)
+	})
+
+	fmt.Println("Starting server on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Server error:", err)
+	}
 }
 
 func readName() string {
 	var name string
-	
+
 	fmt.Print("Enter your name: ")
 	_, err := fmt.Scan(&name)
-	
+
 	if err != nil {
-        fmt.Println("Error reading name:", err)
-    }
-	
+		fmt.Println("Error reading name:", err)
+	}
+
 	return name
 }
