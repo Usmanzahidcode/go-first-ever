c7d63ab6508556b69b7a38bffc14829ea9ba0ae3
Usman Zahid <developerusman@yahoo.com>
2026-01-18 20:49:53 +0500

    Unknown routes handled

--- main.go
+++ main.go
@@ -1,35 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
-func main() {
-	name := readName()
-	fmt.Println("Hello,", name) 
+type Response struct {
+	Message string `json:"message"`
+	ResponseCode int `json:"response_code"`
+}
 
-	// Set up a simple HTTP route
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %s!", name)
+func main() {
+	http.HandleFunc("/", func(reponseWriter http.ResponseWriter, request *http.Request) {
+		// Every un-registered will result in 404
+		if request.URL.Path != "/" {
+			http.NotFound(reponseWriter, request)
+			return
+		}
+	
+		reponseWriter.Header().Set("Content-Type", "text/html")
+		fmt.Fprintf(reponseWriter, "<p>Server up and running!</p>")
 	})
 
-	fmt.Println("Starting server on :8080")
+	http.HandleFunc("/api", func(reponseWriter http.ResponseWriter, request *http.Request) {
+		reponseWriter.Header().Set("Content-Type", "application/json")
+		resp := Response{
+			Message: "Server is up and running!",
+			ResponseCode: 200,
+		}
+		json.NewEncoder(reponseWriter).Encode(resp)
+	})
+
+	fmt.Println("Server running at http://localhost:8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Println("Server error:", err)
 	}
-}
-
-func readName() string {
-	var name string
-
-	fmt.Print("Enter your name: ")
-	_, err := fmt.Scan(&name)
-
-	if err != nil {
-		fmt.Println("Error reading name:", err)
-	}
-
-	return name
 }
